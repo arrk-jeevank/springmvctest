c46bd0342b86ca1bb895770746a61824186a04bf
unknown <jeevankumarr@jeevankumarr.arrkgroup.com>
2014-11-26 12:46:35 +0530

    Context xml

--- src/main/java/com/spring/test/HelloController.java
+++ src/main/java/com/spring/test/HelloController.java
@@ -2,12 +2,14 @@ package com.spring.test;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
  
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
  
 public class HelloController extends AbstractController{
  
 	@Override
+	@RequestMapping("/hello")
 	protected ModelAndView handleRequestInternal(HttpServletRequest request,
 		HttpServletResponse response) throws Exception {
  
